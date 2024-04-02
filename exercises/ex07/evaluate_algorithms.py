b4f0126a7a4de4593fecbf7134de1e262119bdd5
Matthew Warner <wmatt@ad.unc.edu>
2024-04-01 23:28:04 -0400

    ex07

--- exercises/ex07/evaluate_algorithms.py
+++ exercises/ex07/evaluate_algorithms.py
@@ -1,10 +1,11 @@
 __author__ = "730544766"
 
+
 import matplotlib.pyplot as plt
-from exercises.ex07.runtime_analysis_functions import evaluate_runtime
-from exercises.ex07.sort_functions import selection_sort, insertion_sort
+from exercises.ex07.runtime_analysis_functions import evaluate_runtime, evaluate_memory_usage
 START_SIZE: int = 0
 END_SIZE: int = 1000
+
 
 times = evaluate_runtime("selection_sort", START_SIZE, END_SIZE)
 plt.plot(times)
