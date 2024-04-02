e2900fef762d11b4179657416b79c76a58be447d
Matthew Warner <wmatt@ad.unc.edu>
2024-04-01 20:40:29 -0400

    sure

---
+++ exercises/ex07/evaluate_algorithms.py
@@ -0,0 +1,12 @@
+__author__ = "730544766"
+
+import matplotlib.pyplot as plt
+from exercises.ex07.runtime_analysis_functions import evaluate_runtime
+from exercises.ex07.sort_functions import selection_sort, insertion_sort
+START_SIZE: int = 0
+END_SIZE: int = 1000
+
+times = evaluate_runtime("selection_sort", START_SIZE, END_SIZE)
+plt.plot(times)
+plt.title("Runtime Analysis of Selection Sort - 730544766")
+plt.show()
